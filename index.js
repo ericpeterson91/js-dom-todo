bece6473245402e7fcd4f7c0b6c3b51788e28b7d
Eric Peterson <ericpeterson91@live.ca>
2022-09-11 17:52:57 -0600

    setting up todo list

---
+++ index.js
@@ -0,0 +1,24 @@
+const addTask = document.querySelector('.add-task')
+const taskContainer = document.querySelector('.task-container')
+const inputTask = document.querySelector('.input-task')
+
+addTask.addEventListener('click', () => {
+
+    let task = document.createElement('div')
+    task.classList.add('task')
+
+    let li = document.createElement('li')
+    li.innerText = `${inputTask.value}`
+    task.appendChild(li)
+
+    let checkButton = document.createElement('button')
+    checkButton.innerHTML = '<i class="fa-solid fa-check"></i>'
+    checkButton.classList.add('checkTask')
+    task.appendChild(checkButton)
+
+    let deleteButton = document.createElement('button')
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
+    deleteButton.classList.add('deleteTask')
+    task.appendChild(deleteButton)
+
+})
